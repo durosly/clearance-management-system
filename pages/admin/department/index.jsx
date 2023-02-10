29e9b315a5e8f8ca84c0fdf4d0999dce9d6274ff
durosly <slyboydon1@gmail.com>
2023-02-10 04:30:06 +0100

    create department

--- pages/admin/department/index.jsx
+++ pages/admin/department/index.jsx
@@ -1,9 +1,272 @@
+import { useState } from "react";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Alert from "react-bootstrap/Alert";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
+import { FaRegTrashAlt } from "react-icons/fa";
+import AdminLayout from "../../../components/admin/layout/admin-layout";
 import handleSession from "../../../session/handle-session";
 import { ADMIN_LEVEL } from "../../../auth_constants/auth";
-import AdminLayout from "../../../components/admin/layout/admin-layout";
+import CollegeModel from "../../../models/college";
+import DepartmentModel from "../../../models/department";
+import { stringifyDoc } from "../../../lib";
 
-function Department() {
-	return <AdminLayout>Deparment</AdminLayout>;
+function Department({ departmentDB, collegesDB }) {
+	// console.table(departmentDB);
+	const [newDepartment, setNewDepartment] = useState({
+		name: "",
+		college: "",
+		abbr: "",
+		duration: undefined,
+	});
+	const [isLoading, setIsLoading] = useState(false);
+	const [isDeleting, setIsDeleting] = useState(false);
+	const [colleges] = useState(collegesDB);
+	const [departments, setDepartments] = useState(departmentDB);
+	const [showAlert, setShowAlert] = useState({
+		show: false,
+		type: "",
+		msg: "",
+	});
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		setIsLoading(true);
+		// setShowAlert({ show: true, type: "warning", msg: "submitting..." });
+
+		try {
+			const response = await axios.post(
+				"/api/department/create",
+				newDepartment
+			);
+
+			if (response.data.ok) {
+				setShowAlert({
+					show: true,
+					type: "success",
+					msg: "Department created",
+				});
+
+				setDepartments([...departments, response.data.department]);
+				setNewDepartment({
+					name: "",
+					college: "",
+					abbr: "",
+					duration: "",
+				});
+				// setNewDepartmentAbbr("");
+				setIsLoading(false);
+			} else {
+				throw new Error(response.error);
+			}
+
+			// console.log(response);
+			// setIsLoading(false);
+		} catch (error) {
+			setIsLoading(false);
+			setShowAlert({
+				show: true,
+				type: "danger",
+				msg: error.message,
+			});
+		}
+	}
+	async function deleteCollege(id) {
+		if (isDeleting) return;
+		setIsDeleting(true);
+		// setShowAlert({ show: true, type: "warning", msg: "submitting..." });
+
+		try {
+			const response = await axios.delete(`/api/college/${id}/delete`);
+
+			if (response.data.ok) {
+				setShowAlert({
+					show: true,
+					type: "success",
+					msg: "Department Removed",
+				});
+
+				const newDepartmentSet = departments.filter(
+					(c) => c._id !== id
+				);
+
+				setDepartments([...newDepartmentSet]);
+				// setNewDepartment("");
+				// setNewDepartmentAbbr("");
+				setIsDeleting(false);
+			} else {
+				throw new Error(response.error);
+			}
+
+			// console.log(response);
+			// setIsLoading(false);
+		} catch (error) {
+			setIsDeleting(false);
+			setShowAlert({
+				show: true,
+				type: "danger",
+				msg: error.message,
+			});
+		}
+	}
+
+	return (
+		<AdminLayout>
+			<Row className="mt-5">
+				<Col>
+					<Form
+						onSubmit={handleSubmit}
+						className="w-75 md:w-50 mx-auto"
+					>
+						<h2>Add Department</h2>
+						<Alert
+							// className="mt-5"
+							show={showAlert.show}
+							onClose={() =>
+								setShowAlert({ ...showAlert, show: false })
+							}
+							variant={showAlert.type}
+							dismissible
+						>
+							{showAlert.msg}
+						</Alert>
+						<Form.Group
+							className="mb-3"
+							controlId="department-college"
+						>
+							<Form.Label>Department College</Form.Label>
+							<Form.Select
+								aria-label="Default select example"
+								value={newDepartment.college}
+								name="college"
+								onChange={(e) =>
+									setNewDepartment({
+										...newDepartment,
+										[e.target.name]: e.target.value,
+									})
+								}
+							>
+								<option
+									value=""
+									disabled
+								>
+									-- select college --
+								</option>
+								{colleges.map((c) => (
+									<option
+										value={c._id}
+										key={c._id}
+									>
+										{c.name}
+									</option>
+								))}
+							</Form.Select>
+						</Form.Group>
+						<Form.Group
+							className="mb-3"
+							controlId="formBasicEmail"
+						>
+							<Form.Label>Department Name</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Enter department name..."
+								name="name"
+								value={newDepartment.name}
+								onChange={(e) =>
+									setNewDepartment({
+										...newDepartment,
+										[e.target.name]: e.target.value,
+									})
+								}
+							/>
+						</Form.Group>
+						<Form.Group
+							className="mb-3"
+							controlId="college-abbr"
+						>
+							<Form.Label>Department Abbreviation</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="Enter college abbr..."
+								name="abbr"
+								value={newDepartment.abbr}
+								onChange={(e) =>
+									setNewDepartment({
+										...newDepartment,
+										[e.target.name]: e.target.value,
+									})
+								}
+							/>
+						</Form.Group>
+						<Form.Group
+							className="mb-3"
+							controlId="department-duration"
+						>
+							<Form.Label>Department duration (years)</Form.Label>
+							<Form.Control
+								type="number"
+								placeholder="Enter department duration..."
+								name="duration"
+								value={newDepartment.duration}
+								onChange={(e) =>
+									setNewDepartment({
+										...newDepartment,
+										[e.target.name]: e.target.valueAsNumber,
+									})
+								}
+							/>
+						</Form.Group>
+
+						<Button
+							variant="primary"
+							type="submit"
+							disabled={isLoading}
+						>
+							Submit
+						</Button>
+					</Form>
+				</Col>
+			</Row>
+			<hr />
+			<Row>
+				<Col>
+					<Table responsive>
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Name</th>
+								<th>Abbr</th>
+								<th>College</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							{departments.map((c, i) => (
+								<tr key={c._id}>
+									<td>{i + 1}</td>
+									<td>{c.name}</td>
+									<td>{c.abbr}</td>
+									<td>{c.college_name}</td>
+									<td>
+										<Button
+											disabled={isDeleting}
+											onClick={() => deleteCollege(c._id)}
+											variant="danger"
+										>
+											<FaRegTrashAlt />
+										</Button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</Table>
+				</Col>
+			</Row>
+		</AdminLayout>
+	);
 }
 
 export default Department;
@@ -23,11 +286,27 @@ export async function getServerSideProps(context) {
 		};
 	}
 
-	// const colleges = await CollegeModel.find({});
+	const colleges = await CollegeModel.find({});
+	const departmentDB = await DepartmentModel.find({});
+
+	const department = [];
+
+	for (const dept of departmentDB) {
+		const college = await CollegeModel.findById(dept._collegeId);
+
+		department.push({
+			_id: dept._id,
+			name: dept.name,
+			duration: dept.duration,
+			abbr: dept.abbr,
+			college_name: college.name,
+		});
+	}
 
 	return {
 		props: {
-			// collegesDB: stringifyDoc(colleges),
+			collegesDB: stringifyDoc(colleges),
+			departmentDB: stringifyDoc(department),
 		},
 	};
 }
---
+++ pages/api/department/create.js
@@ -0,0 +1,43 @@
+// import CollegeModel from "../../../models/college";
+import DepartmentModel from "../../../models/department";
+import handleSession from "../../../session/handle-session";
+import { ADMIN_LEVEL } from "../../../auth_constants/auth";
+
+async function handler(req, res) {
+	if (req.method === "POST") {
+		try {
+			const user = await handleSession({
+				req,
+				authLevel: [ADMIN_LEVEL],
+			});
+
+			if (!user) throw new Error("Unathorized");
+
+			const { name, abbr, college, duration } = req.body;
+
+			if (!name) {
+				throw new Error("Enter name of college");
+			} else if (!abbr) {
+				throw new Error("Enter college abbrevation");
+			}
+			const department = await DepartmentModel.create({
+				name,
+				abbr,
+				_collegeId: college,
+				duration,
+			});
+
+			// await ProfileModel.create({ _userId: newUser.id });
+
+			// await user.save();
+			res.status(200).json({ ok: true, msg: "Success", department });
+		} catch (error) {
+			res.status(401).json({ ok: false, msg: error.message });
+		}
+	} else {
+		// throw new Error("Invalid request method")
+		res.status(400).json({ ok: false, msg: "invalid request method" });
+	}
+}
+
+export default handler;
