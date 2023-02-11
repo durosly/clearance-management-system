c86b45a9b759f6e76ad3cc4b4d2cc3a75f78154f
durosly <slyboydon1@gmail.com>
2023-02-11 02:38:43 +0100

    adjust form for payment list item creation

--- models/payment-list.js
+++ models/payment-list.js
@@ -1,12 +1,17 @@
 import mongoose from "mongoose";
 
-const paymentListSchema = new mongoose.Schema({
-	title: String,
-	amount: Number,
-	created_at: { type: Date, default: Date.now },
-	_collegeId: String, // all | college _id
-	_departmentId: String, // all | department _id
-});
+const paymentListSchema = new mongoose.Schema(
+	{
+		title: String,
+		amount: Number,
+		_collegeId: String, // all | college _id
+		_departmentId: String, // all | department _id
+		_sessionId: String,
+	},
+	{
+		timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+	}
+);
 
 const PaymentListModel =
 	mongoose?.models?.PaymentList ||
--- pages/admin/payments/index.jsx
+++ pages/admin/payments/index.jsx
@@ -1,7 +1,484 @@
+import { useState } from "react";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Alert from "react-bootstrap/Alert";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
+import Modal from "react-bootstrap/Modal";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { GrEdit } from "react-icons/gr";
 import AdminLayout from "../../../components/admin/layout/admin-layout";
+import handleSession from "../../../session/handle-session";
+import { ADMIN_LEVEL } from "../../../auth_constants/auth";
+import SessionModel from "../../../models/session";
+import CollegeModel from "../../../models/college";
+import DepartmentModel from "../../../models/department";
+import { stringifyDoc } from "../../../lib";
 
-function Payments() {
-	return <AdminLayout>Payments</AdminLayout>;
+function Payments({ sessionsDB, departmentsDB, collegesDB }) {
+	// console.table(departmentDB);
+
+	const [payment, setPayment] = useState({
+		title: "",
+		amount: "",
+		college: "",
+		department: "",
+		session: "",
+	});
+
+	const [newSession, setNewSession] = useState({
+		title: "",
+		year: "",
+	});
+
+	const [showModal, setShowModal] = useState(false);
+	const [edit, setEdit] = useState({ title: "", id: "", level: "" });
+	const [isEditing, setIsEditing] = useState(false);
+
+	const [sessions, setSessions] = useState(sessionsDB);
+	const [departments, setDepartments] = useState([]);
+	const [colleges, setColleges] = useState(collegesDB);
+
+	const [isLoading, setIsLoading] = useState(false);
+	const [isDeleting, setIsDeleting] = useState(false);
+	const [showAlert, setShowAlert] = useState({
+		show: false,
+		type: "",
+		msg: "",
+	});
+
+	async function handleSubmit(e) {
+		e.preventDefault();
+		setIsLoading(true);
+		// setShowAlert({ show: true, type: "warning", msg: "submitting..." });
+
+		try {
+			const response = await axios.post(
+				"/api/session/create",
+				newSession
+			);
+
+			if (response.data.ok) {
+				setShowAlert({
+					show: true,
+					type: "success",
+					msg: "Session created",
+				});
+
+				setSessions([...sessions, response.data.session]);
+				setNewSession({
+					title: "",
+					year: "",
+				});
+				// setNewDepartmentAbbr("");
+				setIsLoading(false);
+			} else {
+				throw new Error(response.error);
+			}
+
+			// console.log(response);
+			// setIsLoading(false);
+		} catch (error) {
+			setIsLoading(false);
+			setShowAlert({
+				show: true,
+				type: "danger",
+				msg: error.message,
+			});
+		}
+	}
+	async function deleteSession(id) {
+		if (isDeleting) return;
+		setIsDeleting(true);
+		// setShowAlert({ show: true, type: "warning", msg: "submitting..." });
+
+		try {
+			const response = await axios.delete(`/api/session/${id}/delete`);
+
+			if (response.data.ok) {
+				setShowAlert({
+					show: true,
+					type: "success",
+					msg: "Department Removed",
+				});
+
+				const newSessionSet = sessions.filter((c) => c._id !== id);
+
+				setSessions([...newSessionSet]);
+				// setNewDepartment("");
+				// setNewDepartmentAbbr("");
+				setIsDeleting(false);
+			} else {
+				throw new Error(response.error);
+			}
+
+			// console.log(response);
+			// setIsLoading(false);
+		} catch (error) {
+			setIsDeleting(false);
+			setShowAlert({
+				show: true,
+				type: "danger",
+				msg: error.message,
+			});
+		}
+	}
+	async function handleEdit(e) {
+		e.preventDefault();
+		if (isEditing) return;
+		setIsEditing(true);
+
+		// console.log({ edit });
+
+		// setShowAlert({ show: true, type: "warning", msg: "submitting..." });
+
+		try {
+			const response = await axios.put(
+				`/api/session/${edit.id}/edit`,
+				edit
+			);
+
+			if (response.data.ok) {
+				setShowAlert({
+					show: true,
+					type: "success",
+					msg: "update successful",
+				});
+
+				const newSessionSet = sessions.map((c) => {
+					if (c._id === edit.id) {
+						c.level = edit.level;
+					}
+
+					return c;
+				});
+
+				setSessions([...newSessionSet]);
+				setEdit({ title: "", id: "", level: "" });
+				setShowModal(false);
+
+				setIsEditing(false);
+			} else {
+				throw new Error(response.error);
+			}
+
+			// console.log(response);
+			// setIsLoading(false);
+		} catch (error) {
+			setIsEditing(false);
+			setShowAlert({
+				show: true,
+				type: "danger",
+				msg: error.message,
+			});
+		}
+	}
+
+	function editSession(id, title, level) {
+		setEdit({ id, title, level });
+		setShowModal(true);
+	}
+
+	function handleCollegeChange(e) {
+		const newD = departmentsDB.filter(
+			(d) => d._collegeId === e.target.value
+		);
+		setDepartments(newD);
+
+		setPayment({
+			...payment,
+			[e.target.name]: e.target.value,
+		});
+	}
+
+	return (
+		<AdminLayout>
+			<Row className="mt-5">
+				<Col>
+					<Form
+						onSubmit={handleSubmit}
+						className="w-75 md:w-50 mx-auto"
+					>
+						<h2>Add payment purpose</h2>
+						<Alert
+							// className="mt-5"
+							show={showAlert.show}
+							onClose={() =>
+								setShowAlert({ ...showAlert, show: false })
+							}
+							variant={showAlert.type}
+							dismissible
+						>
+							{showAlert.msg}
+						</Alert>
+						<Form.Group
+							className="mb-3"
+							controlId="formBasicEmail"
+						>
+							<Form.Label>Purpose of payment</Form.Label>
+							<Form.Control
+								type="text"
+								placeholder="School fees..."
+								value={payment.title}
+								name="title"
+								onChange={(e) =>
+									setPayment({
+										...payment,
+										[e.target.name]: e.target.value,
+									})
+								}
+							/>
+						</Form.Group>
+						<Form.Group
+							className="mb-3"
+							controlId="college-abbr"
+						>
+							<Form.Label>Year of admission</Form.Label>
+							<Form.Control
+								type="number"
+								placeholder="amount..."
+								name="amount"
+								value={payment.amount}
+								onChange={(e) =>
+									setPayment({
+										...payment,
+										[e.target.name]: e.target.value,
+									})
+								}
+							/>
+						</Form.Group>
+						<Form.Group
+							className="mb-3"
+							controlId="academic-session"
+						>
+							<Form.Label>Academic session</Form.Label>
+							<Form.Select
+								aria-label="Default select example"
+								value={payment.session}
+								name="session"
+								onChange={(e) =>
+									setPayment({
+										...payment,
+										[e.target.name]: e.target.value,
+									})
+								}
+							>
+								<option
+									value=""
+									disabled
+								>
+									-- select session --
+								</option>
+								{sessions.map((c) => (
+									<option
+										key={c._id}
+										value={c._id}
+									>
+										{c.title}
+									</option>
+								))}
+							</Form.Select>
+						</Form.Group>
+						<Form.Group
+							className="mb-3"
+							controlId="college"
+						>
+							<Form.Label>College</Form.Label>
+							<Form.Select
+								aria-label="Default select example"
+								value={payment.college}
+								name="college"
+								onChange={handleCollegeChange}
+							>
+								<option
+									disabled
+									selected
+								>
+									-- select college --
+								</option>
+								{colleges.map((c) => (
+									<option
+										key={c._id}
+										value={c._id}
+									>
+										{c.name}
+									</option>
+								))}
+							</Form.Select>
+						</Form.Group>
+						<Form.Group
+							className="mb-3"
+							controlId="department"
+						>
+							<Form.Label>Department</Form.Label>
+							<Form.Select
+								aria-label="Default select example"
+								value={payment.department}
+								name="department"
+								onChange={(e) =>
+									setPayment({
+										...payment,
+										[e.target.name]: e.target.value,
+									})
+								}
+							>
+								<option
+									disabled
+									selected
+								>
+									-- select department --
+								</option>
+								{departments.map((c) => (
+									<option
+										key={c._id}
+										value={c._id}
+									>
+										{c.name}
+									</option>
+								))}
+							</Form.Select>
+						</Form.Group>
+
+						<Button
+							variant="primary"
+							type="submit"
+							disabled={isLoading}
+						>
+							Submit
+						</Button>
+					</Form>
+				</Col>
+			</Row>
+			<hr />
+			<Row>
+				<Col>
+					<Table responsive>
+						<thead>
+							<tr>
+								<th>#</th>
+								<th>Title</th>
+								<th>Year</th>
+								<th>Level</th>
+								<th></th>
+							</tr>
+						</thead>
+						<tbody>
+							{sessions.map((c, i) => (
+								<tr key={c._id}>
+									<td>{i + 1}</td>
+									<td>{c.title}</td>
+									<td>{c.year}</td>
+									<td>{c.level}</td>
+									<td>
+										<Button
+											className="me-2"
+											variant="info"
+											onClick={() =>
+												editSession(
+													c._id,
+													c.title,
+													c.level
+												)
+											}
+										>
+											<GrEdit />
+										</Button>
+										<Button
+											disabled={isDeleting}
+											onClick={() => deleteSession(c._id)}
+											variant="danger"
+										>
+											<FaRegTrashAlt />
+										</Button>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</Table>
+				</Col>
+			</Row>
+
+			<Modal
+				show={showModal}
+				onHide={() => setShowModal(false)}
+				size="md"
+				aria-labelledby="contained-modal-title-vcenter"
+				centered
+			>
+				<Modal.Header closeButton>
+					<Modal.Title id="contained-modal-title-vcenter">
+						Update {edit.title} session Level
+					</Modal.Title>
+				</Modal.Header>
+				<Modal.Body>
+					<Form
+						onSubmit={handleEdit}
+						className="w-75 md:w-50 mx-auto"
+					>
+						<Form.Group>
+							<Form.Select
+								aria-label="Default select example"
+								value={edit.level}
+								onChange={(e) =>
+									setEdit({
+										...edit,
+										level: e.target.value,
+									})
+								}
+							>
+								<option disabled>-- select level --</option>
+								<option value="100">100</option>
+								<option value="200">200</option>
+								<option value="300">300</option>
+								<option value="400">400</option>
+								<option value="500">500</option>
+							</Form.Select>
+						</Form.Group>
+						<Button
+							disabled={isEditing}
+							type="submit"
+							variant="primary"
+						>
+							Save
+						</Button>
+					</Form>
+				</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={() => setShowModal(false)}>Close</Button>
+				</Modal.Footer>
+			</Modal>
+		</AdminLayout>
+	);
 }
 
 export default Payments;
+
+export async function getServerSideProps(context) {
+	const user = await handleSession({
+		req: context.req,
+		authLevel: [ADMIN_LEVEL],
+	});
+
+	if (!user) {
+		return {
+			redirect: {
+				destination: "/",
+				permanent: false,
+			},
+		};
+	}
+
+	const sessions = await SessionModel.find({});
+	const colleges = await CollegeModel.find({});
+	const departments = await DepartmentModel.find({});
+
+	return {
+		props: {
+			sessionsDB: stringifyDoc(sessions),
+			collegesDB: stringifyDoc(colleges),
+			departmentsDB: stringifyDoc(departments),
+		},
+	};
+}
