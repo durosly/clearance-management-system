0fdf98d86e02403817a58eb457392d75ebdb4cc0
durosly <slyboydon1@gmail.com>
2023-03-26 03:18:27 +0100

    add payment session info

--- models/payment-list.js
+++ models/payment-list.js
@@ -1,20 +1,21 @@
 import mongoose from "mongoose";
 
 const paymentListSchema = new mongoose.Schema(
-	{
-		title: String,
-		amount: Number,
-		_collegeId: String, // all | college _id
-		_departmentId: String, // all | department _id
-		_sessionId: String,
-	},
-	{
-		timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
-	}
+    {
+        title: String,
+        amount: Number,
+        _collegeId: String, // all | college _id
+        _departmentId: String, // all | department _id
+        _sessionId: String,
+        sessionInfo: String, // session title (level)
+    },
+    {
+        timestamps: { createdAt: "created_at", updatedAt: "updated_at" },
+    }
 );
 
 const PaymentListModel =
-	mongoose?.models?.PaymentList ||
-	mongoose.model("PaymentList", paymentListSchema);
+    mongoose?.models?.PaymentList ||
+    mongoose.model("PaymentList", paymentListSchema);
 
 export default PaymentListModel;
--- models/payment.js
+++ models/payment.js
@@ -1,21 +1,21 @@
 import mongoose from "mongoose";
 
 const paymentSchema = new mongoose.Schema(
-	{
-		title: String,
-		_userId: String,
-		amount: Number,
-		_collegeId: String,
-		_departmentId: String,
-		_paymentListId: String,
-		reference: String,
-		_sessionId: String,
-		sessionInfo: String,
-	},
-	{ timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
+    {
+        title: String,
+        _userId: String,
+        amount: Number,
+        _collegeId: String,
+        _departmentId: String,
+        _paymentListId: String,
+        reference: String,
+        _sessionId: String,
+        sessionInfo: String, // session title (level)
+    },
+    { timestamps: { createdAt: "created_at", updatedAt: "updated_at" } }
 );
 
 const PaymentModel =
-	mongoose?.models?.Payment || mongoose.model("Payment", paymentSchema);
+    mongoose?.models?.Payment || mongoose.model("Payment", paymentSchema);
 
 export default PaymentModel;
