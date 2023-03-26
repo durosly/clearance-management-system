0de995c23d842f1d78c3bb8fac44f9d1b0338ae9
durosly <slyboydon1@gmail.com>
2023-03-26 03:19:19 +0100

    add payment evaluation to determine complete payment level

--- pages/api/payments/pay-fee.js
+++ pages/api/payments/pay-fee.js
@@ -7,60 +7,77 @@ import { getFullPaymentData } from "../../../lib";
 import { STUDENT_LEVEL } from "../../../auth_constants/auth";
 
 async function handler(req, res) {
-	if (req.method === "POST") {
-		try {
-			const user = await handleSession({
-				req,
-				authLevel: [STUDENT_LEVEL],
-			});
+    if (req.method === "POST") {
+        try {
+            const user = await handleSession({
+                req,
+                authLevel: [STUDENT_LEVEL],
+            });
 
-			if (!user) throw new Error("Unathorized");
+            if (!user) throw new Error("Unathorized");
 
-			const { id, reference, title, amount } = req.body;
+            const { id, reference, title, amount } = req.body;
 
-			if (!title) {
-				throw new Error("Enter title of payment");
-			} else if (!amount) {
-				throw new Error("Enter amount of payment");
-			} else if (!id) {
-				throw new Error("A payment item was not selected");
-			} else if (!reference) {
-				throw new Error("Payment may not have been successful");
-			}
+            if (!title) {
+                throw new Error("Enter title of payment");
+            } else if (!amount) {
+                throw new Error("Enter amount of payment");
+            } else if (!id) {
+                throw new Error("A payment item was not selected");
+            } else if (!reference) {
+                throw new Error("Payment may not have been successful");
+            }
 
-			const paymentList = await PaymentListModel.findById(id);
+            const paymentListItem = await PaymentListModel.findById(id);
 
-			const profile = await ProfileModel.findOne({ _userId: user.id });
-			const session = await SessionModel.findById(profile._sessionId);
+            const profile = await ProfileModel.findOne({ _userId: user.id });
+            const session = await SessionModel.findById(profile._sessionId);
 
-			const payment = await PaymentModel.create({
-				title,
-				amount: paymentList.amount,
-				reference,
-				_collegeId: paymentList._collegeId,
-				_departmentId: paymentList._departmentId,
-				_sessionId: paymentList._sessionId,
-				sessionInfo: `${session.title} (${session.level})`,
-				_userId: user.id,
-				_paymentListId: paymentList.id,
-			});
+            const payment = await PaymentModel.create({
+                title,
+                amount: paymentListItem.amount,
+                reference,
+                _collegeId: paymentListItem._collegeId,
+                _departmentId: paymentListItem._departmentId,
+                _sessionId: paymentListItem._sessionId,
+                sessionInfo: `${session.title} (${session.level})`,
+                _userId: user.id,
+                _paymentListId: paymentListItem.id,
+            });
 
-			const fullData = await getFullPaymentData(payment);
+            const paymentList = await PaymentListModel.find({
+                _collegeId: { $in: ["all", profile._collegeId] },
+                _departmentId: { $in: ["all", profile._departmentId] },
+                _sessionId: profile._sessionId,
+                sessionInfo: `${session.title} (${session.level})`,
+            });
 
-			// await user.save();
-			res.status(200).json({
-				ok: true,
-				msg: "Success",
-				payment: fullData,
-			});
-		} catch (error) {
-			console.log(error.message);
-			res.status(401).json({ ok: false, msg: error.message });
-		}
-	} else {
-		// throw new Error("Invalid request method")
-		res.status(400).json({ ok: false, msg: "invalid request method" });
-	}
+            const paymentsDone = await PaymentModel.find({
+                sessionInfo: `${session.title} (${session.level})`,
+                _userId: user.id,
+            });
+
+            if (paymentList.length === paymentsDone.length) {
+                profile.clearanceLevel = 3;
+                await profile.save();
+            }
+
+            const fullData = await getFullPaymentData(payment);
+
+            // await user.save();
+            res.status(200).json({
+                ok: true,
+                msg: "Success",
+                payment: fullData,
+            });
+        } catch (error) {
+            console.log(error.message);
+            res.status(401).json({ ok: false, msg: error.message });
+        }
+    } else {
+        // throw new Error("Invalid request method")
+        res.status(400).json({ ok: false, msg: "invalid request method" });
+    }
 }
 
 export default handler;
