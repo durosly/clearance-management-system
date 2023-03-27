dc98084be8b355207c0aa2af75e53e1143fcbf3c
durosly <slyboydon1@gmail.com>
2023-03-27 14:43:20 +0100

    remove stage increase by admin

--- pages/api/profile/[id]/update-department.js
+++ pages/api/profile/[id]/update-department.js
@@ -4,43 +4,42 @@ import handleSession from "../../../../session/handle-session";
 import { ADMIN_LEVEL } from "../../../../auth_constants/auth";
 
 async function handler(req, res) {
-	if (req.method === "PUT") {
-		try {
-			const user = await handleSession({
-				req,
-				authLevel: [ADMIN_LEVEL],
-			});
+    if (req.method === "PUT") {
+        try {
+            const user = await handleSession({
+                req,
+                authLevel: [ADMIN_LEVEL],
+            });
 
-			if (!user) throw new Error("Unathorized");
+            if (!user) throw new Error("Unathorized");
 
-			const { college, department } = req.body;
-			const id = req.query.id;
+            const { college, department } = req.body;
+            const id = req.query.id;
 
-			if (!college) {
-				throw new Error("Please, select a college");
-			} else if (!department) {
-				throw new Error("Please, select a department");
-			}
+            if (!college) {
+                throw new Error("Please, select a college");
+            } else if (!department) {
+                throw new Error("Please, select a department");
+            }
 
-			const sessions = await SessionModel.find({});
-			const lastSession = await sessions[sessions.length - 1];
+            const sessions = await SessionModel.find({});
+            const lastSession = await sessions[sessions.length - 1];
 
-			await ProfileModel.findByIdAndUpdate(id, {
-				_collegeId: college,
-				_departmentId: department,
-				_sessionId: lastSession.id,
-				clearanceLevel: 4,
-			});
+            await ProfileModel.findByIdAndUpdate(id, {
+                _collegeId: college,
+                _departmentId: department,
+                _sessionId: lastSession.id,
+            });
 
-			res.status(200).json({ ok: true, msg: "Success" });
-		} catch (error) {
-			console.log(error.message);
-			res.status(401).json({ ok: false, msg: error.message });
-		}
-	} else {
-		// throw new Error("Invalid request method")
-		res.status(400).json({ ok: false, msg: "invalid request method" });
-	}
+            res.status(200).json({ ok: true, msg: "Success" });
+        } catch (error) {
+            console.log(error.message);
+            res.status(401).json({ ok: false, msg: error.message });
+        }
+    } else {
+        // throw new Error("Invalid request method")
+        res.status(400).json({ ok: false, msg: "invalid request method" });
+    }
 }
 
 export default handler;
--- pages/api/profile/[id]/update-status.js
+++ pages/api/profile/[id]/update-status.js
@@ -3,36 +3,35 @@ import handleSession from "../../../../session/handle-session";
 import { ADMIN_LEVEL } from "../../../../auth_constants/auth";
 
 async function handler(req, res) {
-	if (req.method === "PUT") {
-		try {
-			const user = await handleSession({
-				req,
-				authLevel: [ADMIN_LEVEL],
-			});
+    if (req.method === "PUT") {
+        try {
+            const user = await handleSession({
+                req,
+                authLevel: [ADMIN_LEVEL],
+            });
 
-			if (!user) throw new Error("Unathorized");
+            if (!user) throw new Error("Unathorized");
 
-			const { status } = req.body;
-			const id = req.query.id;
+            const { status } = req.body;
+            const id = req.query.id;
 
-			if (!status) {
-				throw new Error("Please, select a status");
-			}
+            if (!status) {
+                throw new Error("Please, select a status");
+            }
 
-			await ProfileModel.findByIdAndUpdate(id, {
-				status,
-				clearanceLevel: 3,
-			});
+            await ProfileModel.findByIdAndUpdate(id, {
+                status,
+            });
 
-			res.status(200).json({ ok: true, msg: "Success" });
-		} catch (error) {
-			console.log(error.message);
-			res.status(401).json({ ok: false, msg: error.message });
-		}
-	} else {
-		// throw new Error("Invalid request method")
-		res.status(400).json({ ok: false, msg: "invalid request method" });
-	}
+            res.status(200).json({ ok: true, msg: "Success" });
+        } catch (error) {
+            console.log(error.message);
+            res.status(401).json({ ok: false, msg: error.message });
+        }
+    } else {
+        // throw new Error("Invalid request method")
+        res.status(400).json({ ok: false, msg: "invalid request method" });
+    }
 }
 
 export default handler;
